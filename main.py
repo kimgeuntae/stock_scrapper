75011df1e8816a947179a1cdbd33c8896ef45a48
kimgeuntae <kok617@naver.com>
2020-04-24 21:34:58 +0900

    Mod param [save_list_to_file]

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from naver_stocks_list import extract_stock_list_tbody, extract_stock_list_thead, extract_stock_detail_url
-from save import save_to_file
+from save import save_list_to_file
 
 MAX_PAGE_NUM = 32
 STOCK_LIST_URL = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0&page="
+
+FILE_FORMAT = "csv"
+FNAME_CAPITALIZATION_RANK = "capitalization_rank"
+FNAME_LOW_VALUATION_LIST= "low_valuation _list"
 
 stocks_table = []
 stocks_table.append(extract_stock_list_thead(f"{STOCK_LIST_URL}{1}"))
@@ -16,4 +20,4 @@ for n in range(MAX_PAGE_NUM):
     for tbody_data in extract_stock_list_tbody(URL):
         stocks_table.append(tbody_data)
 
-save_to_file(stocks_table)
+save_list_to_file(f"{FNAME_CAPITALIZATION_RANK}.{FILE_FORMAT}", stocks_table)
--- save.py
+++ save.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import csv
 
-def save_to_file(stocks):
+def save_list_to_file(file_name, stocks):
 
-    file = open("capitalization_rank.csv", mode="w", encoding="utf-8", newline="")  # encoding - 한글깨짐 , newline - 개행 삭제
+    file = open(file_name, mode="w", encoding="utf-8", newline="")  # encoding - 한글깨짐 , newline - 개행 삭제
     writer = csv.writer(file)
     
     for stock in stocks:
