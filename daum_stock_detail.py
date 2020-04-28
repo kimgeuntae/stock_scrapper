4ee4285cdf81df8ab692b8a7ea71defcf05c48fc
kimgeuntae <kok617@naver.com>
2020-04-28 14:19:27 +0900

    Notice [extract finance data]

--- daum_stock_detail.py
+++ daum_stock_detail.py
@@ -46,11 +46,37 @@ def extract_state_financial_statement_detail(stock_number):
     table = soup.find("table")
     tbody = table.find("tbody")
 
-    trs = []
+    # get finance division , year, quarter
+    finance_div = tbody.find("tr", {"class": "row0"}).find_all("th", {"scope": "colgroup"})
+    yaer_line = int(finance_div[0]["colspan"])
+    quarter_line = int(finance_div[1]["colspan"])
 
-    for i in range(0, 28):
-        trs.append(tbody.find("tr", {"class": f"row{i}"}))
+    
+    # 가로줄 데이터 dict, 1부터 year+quarter 개수 합까지
+    for i in range(1, yaer_line + quarter_line + 1):
+        f"finance_dict{i}" = {}
 
-    print(trs)
+        # 해야할일!!!
+        # 가로 개수만큼 dict 를 만들거고, 거기에 세로 만큼 하나씩 dict 데이터를 넣을거임.
+        # 그리고 완성된 dict 한개씩 list 에 append 할 예정.
+        # 최종 list 에는 yaer_line + quarter_line 개수 만큼 리스트가 있고, 각 가로당 세로줄 데이터 dict 가 들어가게 됨.
+
+        if i == 1:
+            f"finance_dict{i}"
+        
+
+        temp_list = []
+
+        for j in range(1, 35):
+
+            tr = tbody.find("tr", {"class": f"row{j}"})
+            ths = tr.find_all("th")
+
+            for th in ths:
+            
+        
+
+    
+
     
     return "a"
